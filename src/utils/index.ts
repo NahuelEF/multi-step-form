e71e664cf82ab19703f4bb8d0bb4d459641a0306
NahuelEF <nahuelfedele@gmail.com>
2026-04-04 17:57:02 -0300

    feat: add calculatePriceYearly function for pricing calculations

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,8 @@ export function trimClassName(className: string | undefined, ...styles: string[]
 
   return allStyles;
 }
+
+export function calculatePriceYearly(basePrice: number, isYearly: boolean): string {
+  const YEARLY_MULTIPLIER = 10;
+  return isYearly ? `$${basePrice * YEARLY_MULTIPLIER}/yr` : `$${basePrice}/mo`;
+}
