f5121baf8d57a5622f465e5f457e22020bbcb866
NahuelEF <nahuelfedele@gmail.com>
2026-04-04 17:59:43 -0300

    feat: implement Addons component with dynamic pricing and form integration

---
+++ src/pages/Addons/Addons.tsx
@@ -0,0 +1,31 @@
+import { calculatePriceYearly } from '@/utils';
+import { FormData } from '@/validations';
+import { useFormContext } from 'react-hook-form';
+import { Checkbox } from '@/components/Checkbox/Checkbox';
+import { Form } from '@/components/Form/Form';
+import { STEP_THREE } from '@/constants/texts.json';
+
+export const Addons = () => {
+  const { register, watch } = useFormContext<FormData>();
+
+  const billingCycle = watch('billingCycle');
+  const isYearly = billingCycle === 'yearly';
+
+  return (
+    <>
+      <Form.Header title={STEP_THREE.TITLE} description={STEP_THREE.DESCRIPTION} />
+      <Form.Content>
+        {STEP_THREE.ADDONS.map((addOn) => (
+          <Checkbox
+            key={addOn.ID}
+            title={addOn.NAME}
+            description={addOn.DESCRIPTION}
+            price={calculatePriceYearly(addOn.PRICE, isYearly)}
+            value={addOn.VALUE as string}
+            {...register('addOns')}
+          />
+        ))}
+      </Form.Content>
+    </>
+  );
+};
